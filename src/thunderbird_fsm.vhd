3ac6c4c429e17dd8e4daca345cb21cfdf2a2b478
Ksl022205 <c27kyle.litman@afacademy.af.edu>
2025-04-02 11:35:45 -0600

    Update thunderbird_fsm.vhd

--- src/thunderbird_fsm.vhd
+++ src/thunderbird_fsm.vhd
@@ -82,62 +82,47 @@
 --|
 --+----------------------------------------------------------------------------
 library ieee;
-  use ieee.std_logic_1164.all;
-  use ieee.numeric_std.all;
-
-entity thunderbird_fsm is
-    port (
-        i_clk, i_reset  : in    std_logic;
-        i_left, i_right : in    std_logic;
-        o_lights_L      : out   std_logic_vector(2 downto 0);
-        o_lights_R      : out   std_logic_vector(2 downto 0)
-    );
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+entity thunderbird_fsm is 
+  port(
+	i_clk, i_reset : in std_logic;
+	i_left, i_right: in std_logic;
+	o_lights_L     : out std_logic_vector(2 downto 0); -- LA -> 0, LC -> 2
+	o_lights_R     : out std_logic_vector(2 downto 0)  -- RA -> 0, RC -> 2
+  );
 end thunderbird_fsm;
-
-architecture Behavioral of thunderbird_fsm is
-    signal f_q: std_logic_vector(2 downto 0);
-    signal f_d: std_logic_vector(2 downto 0);
-
+architecture thunderbird_fsm_arch of thunderbird_fsm is 
+  -- Constants and state signals
+  signal f_q : std_logic_vector(7 downto 0) := "10000000";
+  signal f_q_next : std_logic_vector(7 downto 0) := "10000000";
 begin
-    process(i_clk, i_reset)
-    begin
-        if i_reset = '1' then
-            f_q <= "000";
-        elsif rising_edge(i_clk) then
-            f_q <= f_d;
-        end if;
-    end process;
-
-    process(f_q, i_left, i_right)
-    begin
-        case f_q is
-            when "000" => -- OFF
-                if i_left = '1' and i_right = '0' then
-                    f_d <= "101";
-                elsif i_right = '1' and i_left = '0' then
-                    f_d <= "010";
-                elsif i_left = '1' and i_right = '1' then
-                    f_d <= "001";
-                else
-                    f_d <= "000";
-                end if;
-            when "001" => f_d <= "000"; -- ON
-            when "010" => f_d <= "011"; -- R1 -> R2
-            when "011" => f_d <= "100"; -- R2 -> R3
-            when "100" => f_d <= "000"; -- R3 -> OFF
-            when "101" => f_d <= "110"; -- L1 -> L2
-            when "110" => f_d <= "111"; -- L2 -> L3
-            when "111" => f_d <= "000"; -- L3 -> OFF
-            when others => f_d <= "000";
-        end case;
-    end process;
-
-    o_lights_L(0) <= (not f_q(2) and not f_q(1) and f_q(0)) or (f_q(2) and f_q(1) and f_q(0));
-    o_lights_L(1) <= (not f_q(2) and not f_q(1) and f_q(0)) or (f_q(2) and f_q(1) and f_q(0)) or (f_q(2) and f_q(1) and not f_q(0));
-    o_lights_L(2) <= (not f_q(2) and not f_q(1) and f_q(0)) or (f_q(2) and f_q(1) and f_q(0)) or (f_q(2) and not f_q(1) and f_q(0)) or (f_q(2) and not f_q(1) and f_q(0));
-    o_lights_R(0) <= (not f_q(2) and not f_q(1) and f_q(0)) or (not f_q(2) and f_q(1) and not f_q(0)) or (not f_q(2) and f_q(1) and f_q(0)) or (f_q(2) and not f_q(1) and f_q(0));
-    o_lights_R(1) <= (not f_q(2) and not f_q(1) and f_q(0)) or (not f_q(2) and f_q(1) and f_q(0)) or (f_q(2) and not f_q(1) and not f_q(0));
-    o_lights_R(2) <= (not f_q(2) and not f_q(1) and f_q(0)) or (f_q(2) and not f_q(1) and not f_q(0));
-
-end Behavioral;
+  -- Next state logic
+  f_q_next(0) <= f_q(1);
+  f_q_next(1) <= f_q(2);
+  f_q_next(2) <= f_q(7) AND i_left AND (NOT i_right);
+  f_q_next(3) <= f_q(4);
+  f_q_next(4) <= f_q(5);
+  f_q_next(5) <= f_q(7) AND (NOT i_left) AND i_right;
+  f_q_next(6) <= f_q(7) AND i_left AND i_right;
+  f_q_next(7) <= (f_q(7) AND (NOT i_left) AND (NOT i_right)) OR f_q(6) OR f_q(3) OR f_q(0);
+  -- Output logic
+  o_lights_R(0) <= f_q(6) OR f_q(5) OR f_q(4) OR f_q(3);
+  o_lights_R(1) <= f_q(6) OR f_q(4) OR f_q(3);
+  o_lights_R(2) <= f_q(6) OR f_q(3);
+  o_lights_L(0) <= f_q(6) OR f_q(2) OR f_q(1) OR f_q(0);
+  o_lights_L(1) <= f_q(6) OR f_q(1) OR f_q(0);
+  o_lights_L(2) <= f_q(6) OR f_q(0);
+  -- State register update
+  register_proc : process (i_clk, i_reset)
+  begin
+    if (rising_edge(i_clk)) then
+      if i_reset = '1' then
+        f_q <= "10000000";
+      else
+        f_q <= f_q_next;
+      end if;
+    end if;
+  end process register_proc;
+end thunderbird_fsm_arch;
 
